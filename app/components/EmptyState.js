865fd8d52bf02005e6fc474045207973ec99ac30
MCES100 <148008714+MCES100@users.noreply.github.com>
2024-01-31 16:21:24 +0000

    main

--- app/components/EmptyState.js
+++ app/components/EmptyState.js
@@ -2,8 +2,12 @@ export default function EmptyState({ setOpen, setPrompt }) {
   return (
     <div className="mt-12 sm:mt-24 space-y-6 text-white text-base mx-8 sm:mx-4 sm:text-2xl leading-12 bg-teal-800 rounded-md py-5 px-5 mb-12 ">
       <p>
-        Powered by Mistral AI models
+        Powered by Mistral AI models.
       </p>
+      <p>
+        Examples: Test you on a upcoming exam, learn new things, have conversations.
+      </p>
+
 
 
     </div>
